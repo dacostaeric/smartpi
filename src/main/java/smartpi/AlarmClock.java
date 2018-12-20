8e7822ed94d70196c40934e6e99edd56ab6ec000
Eric da Costa <eric.dacosta@stud.unibas.ch>
2018-12-20 21:38:19 +0100

    added some basic functionality, it is now possible to set an alarm

--- src/main/java/smartpi/AlarmClock.java
+++ src/main/java/smartpi/AlarmClock.java
@@ -1,3 +1,32 @@
+package smartpi;
+
+import java.time.LocalTime;
+
 public class AlarmClock {
+  public int alarmHour;
+  public int alarmMinute;
+  public int alarmSecond;
+
+  public AlarmClock(int alarmHour, int alarmMinute, int alarmSecond) {
+    this.alarmHour = alarmHour;
+    this.alarmMinute = alarmMinute;
+    this.alarmSecond = alarmSecond;
+  }
+
+  public String getAlarmTime() {
+    return this.alarmHour + ":" + this.alarmMinute + ":" + this.alarmSecond;
+  }
+
+  static String getCurrentTime() {
+    LocalTime localTime = LocalTime.now();
+    return localTime.getHour() + ":" + localTime.getMinute() + ":"
+        + localTime.getSecond();
+  }
+
+  public static void main(String[] args) {
+    AlarmClock alarm = new AlarmClock(21, 36,00);
+    System.out.println(alarm.getAlarmTime());
+    System.out.println(getCurrentTime());
+  }
 
 }
