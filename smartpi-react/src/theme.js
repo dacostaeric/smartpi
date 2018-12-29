a083061ed76351758d7e376445b708c339c7d5eb
Alessandro Pittori <alessandro.pittori@stud.unibas.ch>
2018-12-29 22:23:24 +0100

    Extend theme

--- smartpi-react/src/theme.js
+++ smartpi-react/src/theme.js
@@ -1,19 +1,33 @@
 const theme = {
   fontFamily: "Segoe UI",
-  topRowHeight: "40%",
-  bottomRowHeight: "60%",
-  smallWidth: "34%",
-  largeWidth: "66%",
-  tilePadding: "15px",
+  topRowHeight: "40vw",
+  bottomRowHeight: "60vw",
+  smallWidth: "34vw",
+  largeWidth: "66vw",
+  tilePadding: "1vw",
+  size: {
+    large: "7vw",
+    sub: "3vw",
+    small: "1.5vw"
+  },
   time: {
-    clockSize: "60px",
+    dateTextSize: "2vw",
+    clockSize: "7vw",
+    clockMargin: "1vw",
+    alarmSize: "1.5vw"
   },
   weather: {
-    iconHeight: "100px",
-    temperatureSize: "60px",
-    weatherTextSize: "20px",
-    sensorTextSize: "15px",
-    sensorTextMargin: "10px"
+    iconHeight: "10vw",
+    temperatureSize: "7vw",
+    weatherTextSize: "2vw",
+    sensorTextSize: "1.5vw",
+    sensorTextMargin: "0px"
+  },
+  shopping: {
+    size: {
+      heading: "2vw",
+      item: "1.5vw"
+    }
   }
 };
 
