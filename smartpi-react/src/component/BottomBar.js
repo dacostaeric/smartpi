e58a6e9557ee88c7ebec481e43439997bea90d55
Alessandro Pittori <alessandro.pittori@stud.unibas.ch>
2018-12-30 13:35:12 +0100

    Change bottom bar height to be dynamic, set display to flex

--- smartpi-react/src/component/BottomBar.js
+++ smartpi-react/src/component/BottomBar.js
@@ -3,7 +3,9 @@ import styled from "styled-components"
 import theme from "../theme"
 
 const BottomBar = styled.div`
-height: ${theme.bottomRowHeight};
+display: flex;
+width: 100%;
+float: left;
 `;
 
 export default BottomBar
