6c1609df83fab99351330a8cc398fe5ab8a15fde
Eric da Costa <eric.dacosta@stud.unibas.ch>
2019-01-02 20:19:24 +0100

    moved Temperature class to java folder and added it to the reactinterface

---
+++ src/main/java/smartpi/Temperature.java
@@ -0,0 +1,28 @@
+package smartpi;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Temperature {
+
+    static String line;
+    static float humidity = 0;
+    static float temperature = 0;
+
+    public static void main(String[] args) throws Exception{
+      Runtime runtime = Runtime.getRuntime();
+      Process process = runtime.exec("python3 /home/pi/Desktop/dht11.py");
+      BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+      line = bufferedReader.readLine();
+      // System.out.println(line);
+      String[] data = line.split(" ",2);
+      temperature = Float.parseFloat(data[0]);
+      humidity = Float.parseFloat(data[1]);
+      bufferedReader.close();
+      process.waitFor();
+      System.out.println("Temperature is : " + temperature + " °C.");
+      System.out.println("Humidity is :" + humidity + " %.");
+
+      ReactInterface.writeSensorData(temperature, humidity);
+    }
+}
