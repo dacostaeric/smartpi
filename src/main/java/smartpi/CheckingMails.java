3140a22d8a0d256de6e8cdb9c4d13fe7e0c16616
Eric da Costa <eric.dacosta@stud.unibas.ch>
2019-01-02 20:43:13 +0100

    added checkingmails to the interface

--- src/main/java/smartpi/CheckingMails.java
+++ src/main/java/smartpi/CheckingMails.java
@@ -39,7 +39,7 @@ public class CheckingMails {
                 System.out.println("Subject: " + message.getSubject());
                 System.out.println("From: " + message.getFrom()[0]);
                 System.out.println("Text: " + message.getContent().toString());
-
+                ReactInterface.makeEmail(message.getFrom()[0].toString(), message.getSubject(), "");
             }
 
             //close the store and folder objects
