0d62ddbc100b5e59340255415c721ad01fc8a2a3
AL4CR1TY <al4cr1ty@pokeboxadvance.net>
2019-01-02 15:34:57 +0100

    Add detail pages

---
+++ smartpi-react/src/page/Calendar.js
@@ -0,0 +1,10 @@
+import React from "react"
+import styled from "styled-components"
+
+import theme from "../theme"
+
+const Calendar = (props) => {
+  return <h1>Calendar</h1>
+};
+
+export default Calendar
---
+++ smartpi-react/src/page/Weather.js
@@ -0,0 +1,10 @@
+import React from "react"
+import styled from "styled-components"
+
+import theme from "../theme"
+
+const Weather = (props) => {
+  return <h1>Weather</h1>
+};
+
+export default Weather
