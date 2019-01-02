eb6b08e28a1f42e6b0a53cbbecf61eac0ebb827d
AL4CR1TY <al4cr1ty@pokeboxadvance.net>
2019-01-02 20:15:37 +0100

    Add back to home to weather detail

--- smartpi-react/src/page/Weather.js
+++ smartpi-react/src/page/Weather.js
@@ -3,8 +3,13 @@ import styled from "styled-components"
 
 import theme from "../theme"
 
+import BackToHome from "../component/BackToHome";
+
 const Weather = (props) => {
-  return <h1>Weather</h1>
+  return (<div>
+    <BackToHome/>
+    <h1>Weather</h1>
+  </div>)
 };
 
 export default Weather
