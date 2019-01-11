9ad7a985fceb430c13c808254f18166a7b4254b6
AL4CR1TY <al4cr1ty@pokeboxadvance.net>
2019-01-11 03:06:20 +0100

    Improve server. Add set alarm api.

--- server/server.js
+++ server/server.js
@@ -4,33 +4,82 @@ let util = require("util"),
     url = require("url"),
     filesystem = require("fs"),
     port = 3001;
+const respond = (response, options, content) => {
+  response.writeHeader(200, options);
+  response.write(content);
+  response.end();
+};
+const respondError = (response, error) => {
+  response.writeHeader(500, {"Content-Type": "text/plain"});
+  response.write(error + "\n");
+  response.end();
+};
+const respondFile = (response, options, file) => {
+  response.writeHeader(200, options);
+  response.write(file, "binary");
+  response.end();
+};
+const writeFile = (filepath, content) => {
+  let success = true;
+  filesystem.writeFile(filepath, content, error => {
+    if (error) {
+      success = false;
+    }
+  });
+  return success;
+};
 http.createServer((request, response) => {
   let requestedPath = url.parse(request.url).pathname;
   if (requestedPath === "/") {
     requestedPath = "/index.html";
   }
-  let fullPath = process.cwd() + requestedPath;
-  console.log("requested", requestedPath);
-  console.log(fullPath);
-  filesystem.readFile(fullPath, (error, file) => {
-    if (error) {
-      response.writeHeader(500, {"Content-Type": "text/plain"});
-      response.write(error + "\n");
-      response.end();
-    } else {
-      if (fullPath.indexOf("api") !== -1) {
-        response.writeHeader(200, {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods" : "POST, GET, OPTIONS",
-          "Access-Control-Allow-Headers" : "Origin, X-Requested-With, Content-Type, Accept",
-          "Content-Type": "application/json"
-        });
-      } else {
-        response.writeHeader(200);
+  switch (requestedPath) {
+    case "/api/setalarm":
+      try {
+        let query = url.parse(request.url).query;
+        let decoded = decodeURIComponent(query);
+        let json = JSON.parse(decoded);
+        if (json.hour === undefined || json.minute === undefined || !Number.isInteger(json.hour) || !Number.isInteger(json.minute)) {
+          throw new Error("hour or minute is not defined");
+        }
+        respond(response, {"Content-Type": "application/json"},
+            "{\"success\":" + writeFile("/api/alarm.json", decoded)
+            + ",\"json\":" + decoded + "}\n");
+        break;
+      } catch (error) {
+        respond(response, {"Content-Type": "application/json"},
+            "{\"success\":false}\n");
+        break;
       }
-      response.write(file, "binary");
-      response.end();
-    }
-  });
+    case "/api/sensor.json":
+    case "/api/email.json":
+    case "/api/calendar.json":
+      let fullPath = process.cwd() + requestedPath;
+      filesystem.readFile(fullPath, (error, file) => {
+        if (error) {
+          respondError(response, error);
+        } else {
+          respondFile(response,
+              {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
+                "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
+                "Content-Type": "application/json"
+              },
+              file);
+        }
+      });
+      break;
+    case "/":
+    default:
+      filesystem.readFile(process.cwd() + "/index.html", (error, file) => {
+        if (error) {
+          respondError(response, error);
+        } else {
+          respondFile(response, {"Content-Type": "text/plain"}, file);
+        }
+      });
+  }
+  console.log("requested", requestedPath);
 }).listen(port);
-console.log("running on " + port);
+console.log("Running webserver on port " + port);
