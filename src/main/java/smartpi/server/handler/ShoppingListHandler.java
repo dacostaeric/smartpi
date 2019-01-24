4f45a7b2c4d090044ec17e0815f10188664e8c7a
AL4CR1TY <al4cr1ty@pokeboxadvance.net>
2019-01-24 11:37:57 +0100

    Use org.json. Add ability to add multiple items.

--- src/main/java/smartpi/server/handler/ShoppingListHandler.java
+++ src/main/java/smartpi/server/handler/ShoppingListHandler.java
@@ -4,8 +4,9 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import org.mortbay.util.ajax.JSON;
+import org.json.JSONArray;
 
 /**
  * @version %I%
@@ -21,11 +22,15 @@ public class ShoppingListHandler extends SmartPiHandler implements HttpHandler {
     switch (httpExchange.getRequestURI().getPath()) {
       case "/api/shop/add":
         String query = httpExchange.getRequestURI().getQuery();
-        shoppingList.add(query);
+        shoppingList.addAll(Arrays.asList(query.split(",")));
         respondAPI(httpExchange, ("{\"success\":true,\"query\":\"" + query + "\"}").getBytes());
         break;
+      case "/api/shop/clear":
+        shoppingList.clear();
+        respondAPI(httpExchange, ("{\"success\":true}").getBytes());
+        break;
       default:
-        respondAPI(httpExchange, JSON.toString(shoppingList).getBytes());
+        respondAPI(httpExchange, new JSONArray(shoppingList).toString().getBytes());
     }
   }
 }
