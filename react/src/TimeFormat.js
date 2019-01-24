8f3fe56eba4fc20f31c92341b7247b4afa8a7c19
AL4CR1TY <al4cr1ty@pokeboxadvance.net>
2019-01-24 12:29:17 +0100

    Fix formatting

--- react/src/TimeFormat.js
+++ react/src/TimeFormat.js
@@ -2,8 +2,10 @@ import settings from "./settings";
 
 const TimeFormat = new Intl.DateTimeFormat("en-GB",
     {
-      hour: settings.time.hour12 ? "numeric" : "2-digit", minute: "2-digit",
-      hour12: settings.time.hour12, timeZone: settings.time.timeZone
+      hour: settings.time.hour12 ? "numeric" : "2-digit", 
+      minute: "2-digit",
+      hour12: settings.time.hour12,
+      timeZone: settings.time.timeZone
     });
 
 export default TimeFormat
